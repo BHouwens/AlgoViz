4ec81a58e87a6ba03ad9bbd4bd2a109d3e884013
BHouwens <zhion360@gmail.com>
2015-08-28 08:54:06 +0200

    Naive Bayes set up

--- js/module.js
+++ js/module.js
@@ -1,6 +1,6 @@
 var NaiveBayes = angular.module('NaiveBayes', []);
 
-NaiveBayes.controller('MainCtrl', ['$scope', function($scope){
+NaiveBayes.controller('MainCtrl', ['$scope', 'P', function($scope, P){
 	$scope.sunny = [3, 2];
 	$scope.overcast = [4, 0];
 	$scope.rainy = [2, 3];
@@ -10,4 +10,19 @@ NaiveBayes.controller('MainCtrl', ['$scope', function($scope){
 
 	$scope.yes = yes.reduce(function(x, y){return x + y});
 	$scope.no = no.reduce(function(x, y){return x + y});
+
+	// Algorithm variables
+	$scope.pOfX = P.OfX($scope.sunny, $scope.yes, $scope.no);
+	$scope.pOfXGivenC = P.OfXGivenC($scope.sunny, $scope.yes);
+	$scope.pOfC = P.OfC($scope.yes, $scope.yes, $scope.no);
+	$scope.posterior = P.Posterior($scope.sunny, $scope.yes, $scope.yes, $scope.no);
+
+	$scope.$watch('[sunny[0], yes]', function(newVal, oldVal, scope){
+		if (newVal && scope.sunny[0] <= scope.yes){
+			scope.pOfX = P.OfX(scope.sunny, scope.yes, scope.no);
+			scope.pOfXGivenC = P.OfXGivenC(scope.sunny, scope.yes);
+			scope.pOfC = P.OfC(scope.yes, scope.yes, scope.no);
+			scope.posterior = P.Posterior(scope.sunny, scope.yes, scope.yes, scope.no);
+		}
+	});
 }]);
--- js/services.js
+++ js/services.js
@@ -1,3 +1,20 @@
-NaiveBayes.service('pOfX', function(){
-	this.
+NaiveBayes.service('P', function(){
+	this.OfX = function(weather, yes, no){
+		return (weather[0] + weather[1]) / (yes + no);
+	}
+
+	this.OfXGivenC = function(weather, choice){
+		return weather[0] / choice;
+	}
+
+	this.OfC = function(choice, yes, no){
+		return choice / (yes + no);
+	}
+
+	this.Posterior = function(weather, choice, yes, no){
+		var numerator = (this.OfXGivenC(weather, choice)) * (this.OfC(choice, yes, no)),
+			denom = this.OfX(weather, yes, no);
+
+		return numerator / denom;
+	}
 });
